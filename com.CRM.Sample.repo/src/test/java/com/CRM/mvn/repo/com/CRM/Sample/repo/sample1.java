e2b68cde4b63804b2fa8bd2e4b7b4211ea64a51e
Shreya <Shreya@DESKTOP-9QQ3COO>
2021-03-12 16:49:40 +0530

    sampletest

---
+++ com.CRM.Sample.repo/src/test/java/com/CRM/mvn/repo/com/CRM/Sample/repo/sample1.java
@@ -0,0 +1,12 @@
+package com.CRM.mvn.repo.com.CRM.Sample.repo;
+
+import org.testng.annotations.Test;
+
+public class sample1 {
+	@Test
+	public void test()
+	{
+		System.out.println("hehehehhe");
+	}
+
+}
