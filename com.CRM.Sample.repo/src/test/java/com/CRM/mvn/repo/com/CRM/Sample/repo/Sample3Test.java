ea5e72acc706a7ec6bbd02d35ef6f9847a592fae
Shreya <Shreya@DESKTOP-9QQ3COO>
2021-03-15 13:36:45 +0530

    added product

---
+++ com.CRM.Sample.repo/src/test/java/com/CRM/mvn/repo/com/CRM/Sample/repo/Sample3Test.java
@@ -0,0 +1,12 @@
+package com.CRM.mvn.repo.com.CRM.Sample.repo;
+
+import org.testng.annotations.Test;
+
+public class Sample3Test {
+	@Test
+	public void addedProduct()
+	{
+		System.out.println("ghfhfjhg");
+	}
+
+}
